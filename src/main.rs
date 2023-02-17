2d34be4279629cdbb7f642fbbe622d06024da461
Rakhi Sharma <rakhish1994@gmail.com>
2023-02-17 17:59:26 +0100

    loop through the pattern and match with char

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::time;
+
 use chrono::{Local};
 
 // https://www.w3.org/TR/xml-entity-names/025.html
@@ -16,7 +17,19 @@ fn main() {
     loop {
         let t = Local::now();
         let time = t.format("%H:%M:%S").to_string();
-        println!("{:?}", time);
+        for row in &DIGITS {
+            for c in time.chars() {
+               // now that we have chars of time, we want to match the pattern
+               let col = match c {
+                '0'..='9' => c as usize - '0' as usize, //this will give an integer
+                ':' => 10,
+                 _ => 10,
+               };
+
+               print!("{} ", row[col]);
+            }
+            println!();
+        }
         std::thread::sleep(time::Duration::from_millis(999))
     }
 }
